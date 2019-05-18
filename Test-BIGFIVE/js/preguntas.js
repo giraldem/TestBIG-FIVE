f4fc8b3456cec348887aef07039bc24a8de73db1
TMARKSAS <giraldem@gmail.com>
2019-05-18 13:15:55 -0500

    Prueba

---
+++ Test-BIGFIVE/js/preguntas.js
@@ -0,0 +1,51 @@
+script{ 
+    	var preguntas =  [ 
+    		"1.Creo que soy una persona activa y vigorosa."
+    		"2.No me gusta hacer las cosas razonando demasiado sobre ellas."
+    		"3.Tiendo a implicarme demasiado cuando alguien me cuenta sus problemas."
+    		"4.No me preocupan especialmente las consecuencias que mis actos puedan tener sobre los demás."
+    		"5.Estoy siempre informado sobre lo que sucede en el mundo."
+    		"6.Nunca he dicho una mentira."
+    		"7.No me gustan las actividades que exigen empeñarse y esforzarse hasta el agotamiento."
+   			"8.Tiendo a ser muy reflexivo."
+   			"9.No suelo sentirme tenso."
+   			"10.Noto fácilmente cuándo las personas necesitan mi ayuda."
+   			"11.No recuerdo fácilmente los números de teléfono que son largos."
+   			"12.Siempre he estado completamente de acuerdo con los demás."
+   			"13.Generalmente tiendo a imponerme a las otras personas, más que a condescender con ellas."
+   			"14.Ante los obstáculos grandes, no conviene empeñarse en conseguir los objetivos propios."
+   			"15.Soy más bien susceptible."
+   			"16.No es necesario comportarse cordialmente con todas las personas."
+   			"17.No me siento muy atraído por las situaciones nuevas e inesperadas."
+   			"18.Siempre he resuelto de inmediato todos los problemas que he encontrado."
+   			"19.No me gustan los ambientes de trabajo en los que hay mucha competitividad."
+   			"20.Llevo a cabo las decisiones que he tomado."
+   			"21.No es fácil que algo o alguien me hagan perder la paciencia."
+   			"22.Me gusta mezclarme con la gente."
+   			"23.Toda novedad me entusiasma."
+   			"24.Nunca me he asustado ante un peligro aunque fuera grave."
+   			"25.Tiendo a decidir rápidamente."
+   			"26.Antes de tomar cualquier iniciativa, me tomo tiempo para valorar las posibles consecuencias."
+   			"27.No creo ser una persona ansiosa."
+   			"28.No suelo saber cómo actuar ante las desgracias de mis amigos."
+   			"29.Tengo muy buena memoria."
+   			"30.Siempre he estado absolutamente seguro de todas mis acciones."
+   			"31.En mi trabajo no le concedo especial importancia a rendir mejor que los demás."
+   			"32.No me gusta vivir de manera demasiado metódica y ordenada."
+   			"33.Me siento vulnerable a las criticas de los demás."
+   			"34.Si es preciso, no tengo inconveniente en ayudar a un desconocido."
+   			"35.No me atraen las situaciones en constante cambio."
+   			"36.Nunca he desobedecido las órdenes recibidas, ni siquiera siendo niño."
+   			"37.No me gustan aquellas actividades en las que es preciso ir de un sitio a otro y moverse continuamente."
+
+    	];
+
+    	var respuestas = [
+    	["5", "4", "3", "2", "1"]
+    	];
+
+
+    	var indice_aleatorio= Math.floor (Math.random()*preguntas.length);
+
+    	alert(preguntas[indice_aleatorio]);
+   }
